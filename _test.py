f653af4893cdc137f5a4d4e88f6a87cd0d42479c
Wonbin Son <sonwonbin@B2ns-MacBook-Pro.local>
2025-08-24 14:04:55 -0400

    first best model

---
+++ _test.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+import torch
+
+# 디바이스 선택
+device = "cuda" if torch.cuda.is_available() else (
+    "mps" if getattr(torch.backends, "mps", None) and torch.backends.mps.is_available() else "cpu"
+)
+
+# 모델 불러오기
+model = YOLO("models/best.pt")
+
+# 이미지 추론
+results = model("img4.jpg", device=device, conf=0.25)
+
+# 결과 시각화 및 저장
+results[0].plot(show=True, save=True, filename="result.jpg")
